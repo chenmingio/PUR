2d54a94e1f9a9feb0d05764e4890f69bb618434b
ChenMing <chenming@192.168.0.100>
2019-07-21 10:00:10 +0800

    use pandas for excel2sql transfer

---
+++ src/constant.py
@@ -0,0 +1,17 @@
+# store the constant
+
+# database url string
+db_url = '../nr.db'
+
+
+# excel files names
+
+XLS_FILES = ['nr_collector', 'vendor_data', 'project_data']
+
+NR_SHEETS = ['project_data', 'part_data', 'project_timing', 'sourcing_concept', 'rfq_part', 'rfq_invest', 'nomi_part', 'nomi_invest']
+
+VENDOR_SHEETS = []
+
+PROJECT_SHEETS = []
+
+# tables name
