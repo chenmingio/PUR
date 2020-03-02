30151169faa8e9b6b539b95a3ee98959ee129d56
chenming <chenming1988cn@gmail.com>
2020-03-02 11:53:26 +0800

    update static files

---
+++ app/static/precache-manifest.382f38b60fcf4474c26861d6825dcf8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4369eccc75e65ad11188e1c578360a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abeb386f30eeb105af01",
+    "url": "/static/css/2.50b8c702.chunk.css"
+  },
+  {
+    "revision": "aee1171ee5805f574861",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "abeb386f30eeb105af01",
+    "url": "/static/js/2.5f195922.chunk.js"
+  },
+  {
+    "revision": "b7553c3ea06286ac011310d791025ccd",
+    "url": "/static/js/2.5f195922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee1171ee5805f574861",
+    "url": "/static/js/main.fab0ba02.chunk.js"
+  },
+  {
+    "revision": "c3fe8a266de03a94fb61",
+    "url": "/static/js/runtime-main.fffe46a2.js"
+  },
+  {
+    "revision": "1d4f5e0846309a11253c7cd3118cc287",
+    "url": "/static/media/webdatarocks-icons.1d4f5e08.ttf"
+  },
+  {
+    "revision": "c724f97808cda000affbe55fe2ecdfd9",
+    "url": "/static/media/webdatarocks-icons.c724f978.woff"
+  },
+  {
+    "revision": "e403c1a07e3613b993d756d56f81926a",
+    "url": "/static/media/webdatarocks-icons.e403c1a0.svg"
+  }
+]);
