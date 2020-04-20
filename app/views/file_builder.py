b5b205153c07e0c6ab9a3302ae2a5c23d5418498
chenming <chenming1988cn@gmail.com>
2020-04-20 09:44:15 +0800

    add logging snippet at file_builder.py

--- app/views/file_builder.py
+++ app/views/file_builder.py
@@ -1,8 +1,15 @@
 from app.views import docx_inject, xlsx_inject
 from app.models import sql_nrm
+from config import DOWNLOAD_FOLDER
+import logging
+import os
+
+log_filename = os.path.join(DOWNLOAD_FOLDER, 'logging.txt')
+logging.basicConfig(filename=log_filename, filemode='w', level=logging.INFO)
 
 
 def build_file(filename, project, vendor, part_list):
+    logging.info(f'[download file]: type={filename}, project={project}, vendor={vendor}, parts={part_list}')
     if filename == "nomination letter":
         if bool(part_list) is False:
             part_list = sql_nrm.get_part_list_by_project_vendor(project, vendor)
