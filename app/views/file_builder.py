aff8e3c301b0521b70f78b522d692845fdd5c997
chenming <chenming1988cn@gmail.com>
2020-04-20 11:24:56 +0800

    remove logging and use csv writer

--- app/views/file_builder.py
+++ app/views/file_builder.py
@@ -1,15 +1,20 @@
 from app.views import docx_inject, xlsx_inject
 from app.models import sql_nrm
 from config import DOWNLOAD_FOLDER
-import logging
 import os
+import csv
 
-log_filename = os.path.join(DOWNLOAD_FOLDER, 'logging.txt')
-logging.basicConfig(filename=log_filename, filemode='w', level=logging.INFO)
+log_filename = os.path.join(DOWNLOAD_FOLDER, 'logging.csv')
+
+
+def log_into_csv(filename, project, vendor, part_list):
+    with open(log_filename, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([f'[download file]: type={filename}, project={project}, vendor={vendor}, parts={part_list}'])
 
 
 def build_file(filename, project, vendor, part_list):
-    logging.info(f'[download file]: type={filename}, project={project}, vendor={vendor}, parts={part_list}')
+    log_into_csv(filename, project, vendor, part_list)
     if filename == "nomination letter":
         if bool(part_list) is False:
             part_list = sql_nrm.get_part_list_by_project_vendor(project, vendor)
